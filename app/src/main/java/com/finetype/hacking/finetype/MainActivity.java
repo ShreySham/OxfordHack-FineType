a9ebab65c7757297fc28590a07c1c8d7369a85f9
ShreySquared <shreysquared@gmail.com>
2016-11-20 09:27:43 +0000

    GUI TouchUp

--- app/src/main/java/com/finetype/hacking/finetype/MainActivity.java
+++ app/src/main/java/com/finetype/hacking/finetype/MainActivity.java
@@ -10,8 +10,11 @@ import android.hardware.SensorManager;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import java.lang.reflect.Array;
 import java.sql.Timestamp;
@@ -20,6 +23,7 @@ import java.util.Calendar;
 public class MainActivity extends AppCompatActivity implements SensorEventListener{
 
     private TextView xText, yText, zText;
+    private EditText firstArray, secondArray, thirdArray, fourthArray;
     private Sensor mySensor;
     private SensorManager SM;
     private double xAcc, yAcc, zAcc;
@@ -30,8 +34,9 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     String sentence = "";
     int xCount, yCount;
 
-    char[][] alphabet = new char[][] {{'a','b','c','d','e','f','g'},{'h','i','j','k','l','m','n'},
-                                        {'o','p','q','r','s','t'},{'u','v','w','x','y','z'}};
+    char[][] alphabet = new char[][] {{'a','b','c','d','e','f','g','\0'},{'h','i','j','k','l','m','n','\0'},
+                                        {'o','p','q','r','s','t','\0','\0'},{'u','v','w','x','y','z','\0','\0'},
+                                        {'\0','\0','\0','\0','\0','\0','\0','\0'}};
 
 
 
@@ -52,6 +57,10 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         yText = (TextView)findViewById(R.id.yText);
         zText = (TextView)findViewById(R.id.zText);
 
+        firstArray = (EditText)findViewById(R.id.firstArray);
+        secondArray = (EditText)findViewById(R.id.secondArray);
+        thirdArray = (EditText)findViewById(R.id.thirdArray);
+        fourthArray = (EditText)findViewById(R.id.fourthArray);
 
 
         final TextView textViewOut = (TextView) findViewById(R.id.keyOut);
@@ -71,55 +80,167 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
 
 
+                            //block to determine if phone tilted
                             if(xAcc<-3){
                                 right = true;
                             }else {right = false;}
                             if(xAcc>3){
                                 left = true;
                             }else{left = false;}
-                            if(yAcc-1<-2){
+                            if(yAcc<1){
                                 down = true;
                             }else {down = false;}
-                            if(yAcc>5){
+                            if(yAcc>7){
                                 up = true;
                             }else{up = false;}
 
+                                try{
 
-                            try{
-                                Thread.sleep(200);
-                            }catch (Exception e) {}
+                                    Thread.sleep(50);
 
+                                }catch(Exception e){}
 
                              if(down==true){
                                 yCount--;
+                                 if (yCount == 0){
+                                     firstArray.setEnabled(true);
+                                     secondArray.setEnabled(false);
+                                     thirdArray.setEnabled(false);
+                                     fourthArray.setEnabled(false);
+                                 }
+                                 if (yCount == 1){
+                                     firstArray.setEnabled(false);
+                                     secondArray.setEnabled(true);
+                                     thirdArray.setEnabled(false);
+                                     fourthArray.setEnabled(false);
+                                 }
+                                 if (yCount == 2){
+                                     firstArray.setEnabled(false);
+                                     secondArray.setEnabled(false);
+                                     thirdArray.setEnabled(true);
+                                     fourthArray.setEnabled(false);
+                                 }
+                                 if (yCount == 3){
+                                     firstArray.setEnabled(false);
+                                     secondArray.setEnabled(false);
+                                     thirdArray.setEnabled(false);
+                                     fourthArray.setEnabled(true);
+                                 }
+                                 if(yCount<=0){
+                                     yCount=0;
+
+                                 }
+                                 if(yCount>=3){
+                                     yCount=3;
+
+                                 }
                                 sentence += " "+alphabet[yCount][0];
 
-                                }
+                             }
+                            try{
+
+                                Thread.sleep(50);
+
+                            }catch(Exception e){}
+
                             if(right==true){
                                 xCount++;
+                                if(xCount<=0){
+                                    xCount=0;
+                                }
+                                if(xCount>=6){
+                                    xCount=6;
+                                }
                                 sentence += " "+alphabet[yCount][xCount];
+
+
                             }
+                            try{
+
+                                Thread.sleep(50);
+
+                            }catch(Exception e){}
                              if (left == true) {
                                     xCount--;
                                     sentence += " " + alphabet[yCount][xCount];
-                                }
+                                 if(xCount<=0){
+                                     xCount=0;
+                                 }
+                                 if(xCount>=6){
+                                     xCount=6;
+                                 }
+                                 }
+
 
                             try{
-                                Thread.sleep(200);
-                            }catch (Exception e) {}
 
+                                Thread.sleep(50);
 
+                            }catch(Exception e){}
                             if(up==true){
                                 yCount++;
                                 sentence += " "+alphabet[yCount][0];
+                                if (yCount == 0){
+                                    firstArray.setEnabled(true);
+                                    secondArray.setEnabled(false);
+                                    thirdArray.setEnabled(false);
+                                    fourthArray.setEnabled(false);
+                                }
+                                if (yCount == 1){
+                                    firstArray.setEnabled(false);
+                                    secondArray.setEnabled(true);
+                                    thirdArray.setEnabled(false);
+                                    fourthArray.setEnabled(false);
+                                }
+                                if (yCount == 2){
+                                    firstArray.setEnabled(false);
+                                    secondArray.setEnabled(false);
+                                    thirdArray.setEnabled(true);
+                                    fourthArray.setEnabled(false);
+                                }
+                                if (yCount == 3){
+                                    firstArray.setEnabled(false);
+                                    secondArray.setEnabled(false);
+                                    thirdArray.setEnabled(false);
+                                    fourthArray.setEnabled(true);
+                                }
+                                if(yCount<0){
+                                    yCount=0;
+                                }
+                                if(yCount>3){
+                                    yCount=3;
+                                }
 
-                            } if(left==true){
+                            }
+                            try{
+
+                                Thread.sleep(50);
+
+                            }catch(Exception e){}
+                            if(left==true){
                                 xCount--;
                                 sentence += " "+alphabet[yCount][xCount];
+                            if(xCount<=0){
+                                xCount=0;
                             }
+                            if(xCount>=6){
+                                xCount=6;
+                            }
+                            }
+                            try{
+
+                                Thread.sleep(50);
+
+                            }catch(Exception e){}
                             if(right==true){
                                 xCount++;
                                 sentence += " "+alphabet[yCount][xCount];
+                            if(xCount<=0){
+                                xCount=0;
+                            }
+                            if(xCount>=6){
+                                xCount=6;
+                            }
                             }
 
 
