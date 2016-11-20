02b555088c8a889550af27222282f92ff1643509
ShreySquared <shreysquared@gmail.com>
2016-11-20 05:31:30 +0000

    Velocity and Position Output

--- app/src/main/java/com/finetype/hacking/finetype/MainActivity.java
+++ app/src/main/java/com/finetype/hacking/finetype/MainActivity.java
@@ -1,20 +1,31 @@
 package com.finetype.hacking.finetype;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.hardware.Sensor;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorEvent;
 import android.hardware.SensorManager;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+
+import java.lang.reflect.Array;
+import java.sql.Timestamp;
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener{
 
     private TextView xText, yText, zText;
     private Sensor mySensor;
     private SensorManager SM;
+    private double xAcc, yAcc, zAcc;
+    public double xVel, yVel, zVel;
+    public double xPos, yPos, zPos;
+    private double lastTime = System.currentTimeMillis();
 
 
 
@@ -26,8 +37,9 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         //create sensor manager
         SM = (SensorManager)getSystemService(SENSOR_SERVICE);
         //accelerometer sensor
-        mySensor = SM.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mySensor = SM.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
         //register sensor listener
+
         SM.registerListener(this, mySensor, SensorManager.SENSOR_DELAY_NORMAL);
         //Assign TextView
         xText = (TextView)findViewById(R.id.xText);
@@ -42,16 +54,42 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         keyPress.setOnClickListener(new View.OnClickListener(){
             @Override
             public  void onClick(View view){
-                textViewOut.setText("BOOM!");
+
             }
         });
+
+             keyPress.setOnTouchListener(
+                    new RelativeLayout.OnTouchListener() {
+                        public boolean onTouch(View v, MotionEvent m) {
+                            double deltaTime= (System.currentTimeMillis() - lastTime);
+                            if (deltaTime<25.0){
+                                deltaTime=0.0;
+                            }
+                            xVel += (xAcc * deltaTime / 1000);
+                            xPos += (xVel * deltaTime / 1000);
+                            yVel += (yAcc * deltaTime / 1000);
+                            yPos += (yVel * deltaTime / 1000);
+                            zVel += (zAcc * deltaTime / 1000);
+                            zPos += (zVel * deltaTime / 1000);
+                            textViewOut.setText("X velocity: " + xVel+"  X position: " + xPos+" Y velocity: " + yVel+"  Y position: " + yPos +" Z velocity: " + zVel+"  Z position: " + zPos);
+                            lastTime=System.currentTimeMillis();
+                            return true;
+                        }
+                    }
+            );
+
+
+
 
     }
     @Override
     public void onSensorChanged(SensorEvent event) {
         xText.setText("X " + event.values[0]);
+        xAcc =  event.values[0];
         yText.setText("Y " + event.values[1]);
+        yAcc = event.values[1];
         zText.setText("Z " + event.values[2]);
+        zAcc = event.values[2];
     }
     @Override
     public void onAccuracyChanged(Sensor sensor, int i) {
